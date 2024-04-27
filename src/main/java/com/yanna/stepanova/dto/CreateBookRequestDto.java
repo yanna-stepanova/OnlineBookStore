de88635af2933c4822b0b6842c9b9717bad45a24
Yana Stepanova <yanna.stepanova@gmail.com>
2024-04-27 21:36:42 +0300

    fixed code-style

--- src/main/java/com/yanna/stepanova/dto/CreateBookRequestDto.java
+++ src/main/java/com/yanna/stepanova/dto/CreateBookRequestDto.java
@@ -3,9 +3,8 @@ package com.yanna.stepanova.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.hibernate.validator.constraints.ISBN;
-
 import java.math.BigDecimal;
+import org.hibernate.validator.constraints.ISBN;
 
 public record CreateBookRequestDto(@NotBlank String title,
                                    @NotBlank String author,
