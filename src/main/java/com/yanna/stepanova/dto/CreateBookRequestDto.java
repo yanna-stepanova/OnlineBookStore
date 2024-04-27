85e9885395376b1e2abff0e288e0113ea2a6c611
Yana Stepanova <yanna.stepanova@gmail.com>
2024-04-27 21:34:26 +0300

    added field `isbn` to the requestDto and its validation

--- src/main/java/com/yanna/stepanova/dto/CreateBookRequestDto.java
+++ src/main/java/com/yanna/stepanova/dto/CreateBookRequestDto.java
@@ -3,9 +3,12 @@ package com.yanna.stepanova.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import org.hibernate.validator.constraints.ISBN;
+
 import java.math.BigDecimal;
 
 public record CreateBookRequestDto(@NotBlank String title,
                                    @NotBlank String author,
+                                   @ISBN String isbn,
                                    @NotNull @Positive BigDecimal price,
                                    String description, String coverImage) {}
