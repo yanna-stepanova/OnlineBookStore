b3417b9bc0d2425fe03a6dccb8d9b5325ae83c77
Yana Stepanova <yanna.stepanova@gmail.com>
2024-05-02 14:38:51 +0300

    converted class of requestDto to record

--- src/main/java/com/yanna/stepanova/dto/user/UserRegistrationRequestDto.java
+++ src/main/java/com/yanna/stepanova/dto/user/UserRegistrationRequestDto.java
@@ -3,28 +3,15 @@ package com.yanna.stepanova.dto.user;
 import com.yanna.stepanova.validation.FieldsValueMatch;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-@Getter
-@Setter
 @FieldsValueMatch(field = "password",
         fieldMatch = "repeatPassword",
         message = "These passwords must match")
-public class UserRegistrationRequestDto {
-    @NotBlank
-    @Email
-    private String email;
-    @NotBlank
-    @Length(min = 8, max = 25)
-    private String password;
-    @NotBlank
-    @Length(min = 8, max = 25)
-    private String repeatPassword;
-    @NotBlank
-    private String firstName;
-    @NotBlank
-    private String lastName;
-    private String shippingAddress;
-}
+public record UserRegistrationRequestDto(
+        @NotBlank @Email String email,
+        @NotBlank @Length(min = 8, max = 25) String password,
+        @NotBlank @Length(min = 8, max = 25) String repeatPassword,
+        @NotBlank String firstName,
+        @NotBlank String lastName,
+        String shippingAddress) {}
