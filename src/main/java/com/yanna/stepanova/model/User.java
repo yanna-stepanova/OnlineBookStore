10513046ed6c47c63882e2bcf679210e8867086d
Yana Stepanova <yanna.stepanova@gmail.com>
2024-05-09 16:48:33 +0300

    fixed code-style

--- src/main/java/com/yanna/stepanova/model/User.java
+++ src/main/java/com/yanna/stepanova/model/User.java
@@ -1,7 +1,15 @@
 package com.yanna.stepanova.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
