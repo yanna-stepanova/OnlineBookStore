853ef9c5ca2e8acf240ca24397fb31fb61f4dd9c
Yana Stepanova <yanna-stepanova@users.noreply.github.com>
2024-05-14 15:16:24 +0300

    added solution to the task (#11)
    * added solution to the task
    
    * some fix
    
    * added fixes after review, but had problem with value "${jwt.secret}"
    
    * fixed problem with variables 'jwt.secret' & 'jwt.expiration'

---
+++ src/main/java/com/yanna/stepanova/security/AuthenticationService.java
@@ -0,0 +1,23 @@
+package com.yanna.stepanova.security;
+
+import com.yanna.stepanova.dto.user.UserLoginRequestDto;
+import com.yanna.stepanova.dto.user.UserLoginResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(requestDto.email(), requestDto.password()));
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
