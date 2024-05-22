9a0a5895f449d1b3dc597522e38ab91043995e57
Yana Stepanova <yanna.stepanova@gmail.com>
2024-05-22 23:04:28 +0300

    clean code

--- src/main/java/com/yanna/stepanova/model/CartItem.java
+++ src/main/java/com/yanna/stepanova/model/CartItem.java
@@ -20,7 +20,7 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.EAGER)//?
+    @ManyToOne
     @JoinColumn(name = "shopcart_id", nullable = false)
     private ShoppingCart shopcart;
     @ManyToOne
