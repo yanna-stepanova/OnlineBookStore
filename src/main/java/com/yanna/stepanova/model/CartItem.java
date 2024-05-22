7ff445742b19e88d2c6c0a14f6e41833f29a9c06
Yana Stepanova <yanna.stepanova@gmail.com>
2024-05-22 23:06:23 +0300

    clean code style

--- src/main/java/com/yanna/stepanova/model/CartItem.java
+++ src/main/java/com/yanna/stepanova/model/CartItem.java
@@ -2,7 +2,6 @@ package com.yanna.stepanova.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
