dde53e573a23d13f2ec15168e2d95e5f1f449818
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-18 16:48:08 +0300

    started BookControllerTest after losing of data

--- src/test/java/com/yanna/stepanova/controller/BookControllerTest.java
+++ src/test/java/com/yanna/stepanova/controller/BookControllerTest.java
@@ -3,26 +3,33 @@ package com.yanna.stepanova.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yanna.stepanova.dto.book.BookDto;
 import com.yanna.stepanova.dto.book.CreateBookRequestDto;
+import jakarta.servlet.ServletException;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Set;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,22 +40,42 @@ class BookControllerTest {
     private ObjectMapper objectMapper;
 
     @BeforeAll
-    static void beforeAll(@Autowired WebApplicationContext appContext) {
+    static void beforeAll(@Autowired DataSource dataSource,
+                          @Autowired WebApplicationContext appContext) throws SQLException {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(appContext)
                 .apply(springSecurity())
                 .build();
+        teardown(dataSource);
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource("database/controller/add-three-entities-in-books-and-books_categories.sql"));
+        }
+    }
+
+    @AfterAll
+    static void afterAll(@Autowired DataSource dataSource) {
+        teardown(dataSource);
+    }
+
+    @SneakyThrows
+    static void teardown(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(true);
+            ScriptUtils.executeSqlScript(connection,
+                    new ClassPathResource("database/controller/remove-all-from-books-and-books_categories.sql"));
+        }
     }
 
     @Test
     @DisplayName("Create a new book")
-    @WithMockUser(username = "admin", roles = {"ADMIN"})// when '//' -> 401
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     public void createBook_ValidRequestDto_Success() throws Exception {
         //given
         CreateBookRequestDto requestDto = new CreateBookRequestDto("New title", "New author",
-                "123-12-12345678", BigDecimal.valueOf(123.45), Set.of(1L, 2L),
+                "978-161-729-045-9", BigDecimal.valueOf(123.45), Set.of(1L, 2L),
                 "New description", "new cover image");
-
         BookDto expected = new BookDto();
         expected.setTitle(requestDto.title());
         expected.setAuthor(requestDto.author());
@@ -57,13 +84,38 @@ class BookControllerTest {
         expected.setDescription(requestDto.description());
         expected.setCoverImage(requestDto.coverImage());
         expected.setCategoryIds(requestDto.categoryIds());
-
         //when
         MvcResult result = mockMvc.perform(post("/books")
                 .content(objectMapper.writeValueAsString(requestDto))
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
+                .andExpect(status().is2xxSuccessful())
                 .andReturn();
         //then
+        BookDto actual = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+        Assertions.assertNotNull(actual);
+        Assertions.assertNotNull(actual.getId());
+        Assertions.assertTrue(EqualsBuilder.reflectionEquals(expected, actual, "id"));
+    }
+
+    @Test
+    @DisplayName("Exception: if get a book by non existing id")
+    @WithMockUser(username = "somebody", roles = {"ADMIN, ROLE"})
+    public void getBookById_NonExistingId_ThrowException() throws Exception {
+        //given
+        Long bookId = 100L;
+        //when
+        try {
+            MvcResult result = mockMvc.perform(get("/books/{id}", bookId)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().is2xxSuccessful())
+                    .andReturn();
+            BookDto actual = objectMapper.readValue(result.getResponse().getContentAsString(), BookDto.class);
+            //then
+            Assertions.assertNull(actual, String.format("Id=%s should be non-existing", bookId));
+        } catch (ServletException exception) {
+            //then
+            Assertions.assertEquals(String.format("Book with id: %s not found", bookId),
+                    exception.getRootCause().getMessage());
+        }
     }
 }
---
+++ src/test/resources/database/controller/add-three-entities-in-books-and-books_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO books VALUES
+ (1, 'First book', 'Writer A', '000-00-00000001', 1.23, 0, 'Book 1', 'http://example.com/cover_1.jpg'),
+ (2, 'Second book', 'Writer B', '000-00-00000002', 2.34, 0,'Book 2', 'http://example.com/cover_2.jpg'),
+ (3, 'Third book', 'Writer C', '000-00-00000003', 3.45, 0,'Book 3', 'http://example.com/cover_3.jpg');
+INSERT INTO books_categories VALUES
+(1, 1),
+(2, 2),
+(3, 3);
