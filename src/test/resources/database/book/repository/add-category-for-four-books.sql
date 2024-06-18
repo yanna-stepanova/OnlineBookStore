c0affd102170876c1bed36843f2a0edaad0618c3
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-19 01:23:28 +0300

    fixed

--- src/test/resources/database/book/repository/add-category-for-four-books.sql
+++ src/test/resources/database/book/repository/add-category-for-four-books.sql
@@ -1,6 +1,7 @@
 INSERT INTO categories VALUES
  (1, 'One category', 'Some category 1', 0),
- (2, 'Two category', 'Some category 2', 0);
+ (2, 'Two category', 'Some category 2', 0),
+ (3, 'Three category', 'Some category 3', 0);
 INSERT INTO books_categories VALUES
 (2, 2),
 (3, 2),
