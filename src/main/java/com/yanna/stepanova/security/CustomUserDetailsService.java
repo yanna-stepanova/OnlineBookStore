53618f1782025b5ccba48dd3a59d3d7780961759
Yana Stepanova <yanna-stepanova@users.noreply.github.com>
2025-04-24 18:50:45 +0300

    Update CustomUserDetailsService.java

--- src/main/java/com/yanna/stepanova/security/CustomUserDetailsService.java
+++ src/main/java/com/yanna/stepanova/security/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepo;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws EntityNotFoundException {
-        return userRepo.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(
-                String.format("Can't find user by such email: %s", email)));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("Can't find user by such email: %s", username)));
     }
 }
